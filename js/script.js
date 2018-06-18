a1cf32a5fb9cfee692bdd1c72168929bf0a52efc
남현우 <deokgu4624@gmail.com>
2018-06-19 00:41:28 +0900

    로딩화면

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-$(".phrase").fadeIn(3000);
-$(".menu").delay(5000).fadeIn("slow");
-$(".name").delay(5000).fadeIn("slow");
-$(".director").delay(5000).fadeIn("slow");
+// $(".phrase").fadeIn(3000);
+// $(".menu").delay(5000).fadeIn("slow");
+// $(".name").delay(5000).fadeIn("slow");
+// $(".director").delay(5000).fadeIn("slow");
+$(".preloader").delay(3000).fadeOut("slow");
