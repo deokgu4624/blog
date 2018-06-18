af24e75cf5d7e1ddaba3c21e0097e0ec3933498c
DESKTOP-G86FUVU\deokg <deokgu4624@gmail.com>
2018-06-19 01:06:02 +0900

    로딩화면 영상으로 교체

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@
 // $(".menu").delay(5000).fadeIn("slow");
 // $(".name").delay(5000).fadeIn("slow");
 // $(".director").delay(5000).fadeIn("slow");
-$(".preloader").delay(3000).fadeOut("slow");
+$(".bnl").delay(3000).fadeOut("slow");
