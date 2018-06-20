9c692f6224318ad1db43ee6a110510cd2848eb1a
남현우 <deokgu4624@gmail.com>
2018-06-21 01:34:18 +0900

    메뉴효과

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ $(".bnl").delay(3000).fadeOut("slow");
 $(".menu").delay(5000).fadeIn("slow");
 $(".name").delay(5000).fadeIn("slow");
 $(".director").delay(5000).fadeIn("slow");
+$("li").mouseover(function(){
+    $(this).addClass("selected");
+})
+$("li").mouseleave(function(){
+    $(this).removeClass("selected");
+})
