32b9d3e56de4a9a6ad685c713ecb20f13f842f45
남현우 <deokgu4624@gmail.com>
2018-06-21 00:55:15 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(".bnl").delay(3000).fadeOut("slow");
-$(".phrase").delay(5000).fadeIn("slow");
 $(".menu").delay(5000).fadeIn("slow");
-$(".name").delay(5000).fadeIn(3000);
-$(".director").delay(5000).fadeIn(3000);
+$(".name").delay(5000).fadeIn("slow");
+$(".director").delay(5000).fadeIn("slow");
